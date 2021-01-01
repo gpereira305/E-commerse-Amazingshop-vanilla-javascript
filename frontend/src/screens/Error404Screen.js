3c9e7b630b773cff11d341fbd54cbd12ba6e837f
Giovane Pereira <giovane305@gmail.com>
2021-01-01 17:15:19 -0300

    dynamic data finished

---
+++ frontend/src/screens/Error404Screen.js
@@ -0,0 +1,12 @@
+ 
+
+
+
+const Error404Screen = {
+    render: () => {
+        return `<div>Page not found!</div>`
+    },
+
+};
+
+export default Error404Screen;
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+
+
+
+const ProductScreen = {
+    render: () => {
+        return `<div>ProductScreen</div>`
+    },
+
+};
+
+export default ProductScreen;
